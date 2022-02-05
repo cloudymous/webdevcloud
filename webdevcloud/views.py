2d541575b31d193e33645a4e7704f2bfec150301
cloudymous <sayacloud@gmail.com>
2022-02-05 13:38:06 +0800

    create about page

--- webdevcloud/views.py
+++ webdevcloud/views.py
@@ -5,7 +5,7 @@ def index(request):
     return HttpResponse("<h1> Index </h1>")
 
 def about(request):
-    return HttpResponse("<h1> Ini adalah About </h1>")
+    return render(request, 'about.html')
 
 def homepage(request):
     return render(request, 'index.html')
