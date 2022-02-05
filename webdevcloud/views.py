1c49bd6a10cc093b2124bb0f14e3c0ab3cbbc6b3
cloudymous <sayacloud@gmail.com>
2022-02-05 13:29:55 +0800

    Create index homepage

--- webdevcloud/views.py
+++ webdevcloud/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
     return HttpResponse("<h1> Index </h1>")
 
 def about(request):
     return HttpResponse("<h1> Ini adalah About </h1>")
+
+def homepage(request):
+    return render(request, 'index.html')
