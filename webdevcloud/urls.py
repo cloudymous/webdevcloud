ac78217fc08a5bc186ec5359589f54bd125acb70
cloudymous <sayacloud@gmail.com>
2022-02-06 23:42:39 +0800

    Add stats app

--- webdevcloud/urls.py
+++ webdevcloud/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path
 
 from . import views
+from stats import views as statVies
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homepage),
     path('about/', views.about),
+    path('stats/', statVies.index),
 ]
