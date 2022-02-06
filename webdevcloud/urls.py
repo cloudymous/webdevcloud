232bc32e5738249ddb93fb44c109d69a89264d2c
cloudymous <sayacloud@gmail.com>
2022-02-07 00:05:40 +0800

    add url path to stats/individual

--- webdevcloud/urls.py
+++ webdevcloud/urls.py
@@ -1,12 +1,12 @@
+from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from . import views
-from stats import views as statVies
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homepage),
     path('about/', views.about),
-    path('stats/', statVies.index),
+    path('stats/', include('stats.urls')),
 ]
